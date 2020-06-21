4875b68efea92378989c40530ca9668e312293ae
Joel Lee <lee.joel395@gmail.com>
2020-06-20 19:52:37 -0700

    Updated people to look like people. Added animation controllers

--- TiltedShed22/Assets/_Scripts/People.cs
+++ TiltedShed22/Assets/_Scripts/People.cs
@@ -11,9 +11,17 @@ public class People : MonoBehaviour
     private int _biteCharge = 1;
     [SerializeField]
     private int _value = 10;
+    [SerializeField]
+    private Animator _animator;
+
+    public void Start() {
+        _animator.SetBool("isDead", false);
+    }
 
     public void OnTriggerEnter2D(Collider2D collision) {
         if (collision.gameObject.CompareTag("Player")) {
+            _animator.SetBool("isDead", true);
+            GetComponent<Collider2D>().enabled = false;
             collision.GetComponent<PlayerController>().StompPeople(_value);
         }
     }
