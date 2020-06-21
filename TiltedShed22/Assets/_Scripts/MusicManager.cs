fa429fa6f244d8ef694053b70eb45154e66816eb
Michael Wolf <michaelwolf95@gmail.com>
2020-06-20 20:38:25 -0700

    Music stuff

---
+++ TiltedShed22/Assets/_Scripts/MusicManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicManager : MonoBehaviour
+{
+    public AudioSource introSource;
+    public AudioSource loopSource;
+    public AudioClip introClip;
+    public AudioClip loopClip;
+    public float delay = -0.2f;
+    
+    private void Start()
+    {
+        loopClip.LoadAudioData();
+        introSource.clip = introClip;
+        introSource.loop = false;
+        introSource.Play();
+        //musicSource.PlayOneShot(introClip, musicSource.volume);
+
+        Invoke("PlayLoop", introClip.length + delay);
+    }
+
+    private void PlayLoop()
+    {
+        //introSource.Stop();
+        loopSource.clip = loopClip;
+        loopSource.loop = true;
+        loopSource.Play();
+    }
+
+
+    public void StopMusic()
+    {
+        introSource.Stop();
+        loopSource.Stop();
+    }
+
+}
