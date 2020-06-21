8e429eba08435c7e638ee8d9d366032c37ada089
Michael Wolf <michaelwolf95@gmail.com>
2020-06-20 17:07:09 -0700

    Paths system

---
+++ TiltedShed22/Assets/_Scripts/LevelGenerator.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelGenerator : MonoBehaviour
+{
+    public GameObject basicObstaclePrefab;
+    [SerializeField] private Path[] paths;
+    
+    public void StartGenerator()
+    {
+        
+    }
+
+    public void StopGenerator()
+    {
+        
+    }
+
+    
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            TestPath(0);
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            TestPath(1);
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            TestPath(2);
+        }
+    }
+    
+    public void TestPath(int pathIndex)
+    {
+        GameObject go = GameObject.Instantiate(basicObstaclePrefab);
+        PathFollower pathFollower = go.GetComponent<PathFollower>();
+        pathFollower.StartPath(paths[pathIndex]);
+    }
+    
+}
---
+++ TiltedShed22/Assets/_Scripts/PathFollower.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Path
+{
+    public Transform startPoint;
+    public Transform midGroundPoint; // 
+    public Transform endPoint;
+
+    public float duration;
+    public AnimationCurve posCurve;
+    public AnimationCurve scaleCurve;
+}
+
+public class PathFollower : MonoBehaviour
+{
+    [SerializeField] private GameObject root;
+    
+    private Path path;
+    private Coroutine followPathCoroutine;
+    
+    public void StartPath(Path argPath)
+    {
+        path = argPath;
+        followPathCoroutine = StartCoroutine(CoFollowPath());
+    }
+
+    private IEnumerator CoFollowPath()
+    {
+       // float scaleStart = 0f;
+       // float scaleMid = 1f;
+
+        float timer = 0f;
+        while (timer < path.duration)
+        {
+            if (Time.timeScale > 0f)
+            {
+                timer += Time.deltaTime;
+
+                float scaleLerp = path.scaleCurve.Evaluate(timer/path.duration);
+                float posLerp = path.posCurve.Evaluate(timer/path.duration);
+
+                root.transform.position = Vector3.Lerp(path.startPoint.position, path.endPoint.position, posLerp);
+                root.transform.localScale = Vector3.LerpUnclamped(Vector3.zero, Vector3.one, scaleLerp);
+
+                yield return null;
+            }
+        }
+
+        followPathCoroutine = null;
+        
+        Destroy(gameObject);
+    }
+    
+}
