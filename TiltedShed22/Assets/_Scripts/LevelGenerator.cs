6f70bd78b26e00a7f8768ca936258712e89ffae1
Michael Wolf <michaelwolf95@gmail.com>
2020-06-20 21:20:25 -0700

    UHH

--- TiltedShed22/Assets/_Scripts/LevelGenerator.cs
+++ TiltedShed22/Assets/_Scripts/LevelGenerator.cs
@@ -12,6 +12,10 @@ public class LevelGenerator : MonoBehaviour
     
     [SerializeField] private Path[] paths;
 
+    public float peopleSkewChance = 50f;
+    public float obstacleSkewChance = 50f;
+    public float obstacleDoubleChance = 75f;
+
     private Coroutine _generatorCoroutine;
     
     public void StartGenerator()
@@ -36,7 +40,10 @@ public class LevelGenerator : MonoBehaviour
         int currPeopleStringLength = 0;
         int lastPeoplePathIndex = 0;
         
-        
+        int obstacleStringGap = 0;
+        int obstacleStringLength = Random.Range((int) 2, 5);
+        int currObstacleStringLength = 0;
+        int lastObstaclePathIndex = 0;
         
         float lastGenTime = 0f;
         float time = 0f;
@@ -44,9 +51,12 @@ public class LevelGenerator : MonoBehaviour
         {
             time += Time.deltaTime;
 
+            int obstaclePath = -1;
             int personPath = -1;
             if (time - lastGenTime > generationTimeStep)
             {
+                lastGenTime = time;
+                
                 // Generate something.
                 if (peopleStringGap <= 0)
                 {
@@ -54,25 +64,110 @@ public class LevelGenerator : MonoBehaviour
                     {
                         peopleStringLength = Random.Range((int) 3, 7);
                         peopleStringGap = Random.Range((int) 5, 10);
-                        lastPeoplePathIndex = Random.Range(0, peoplePrefabs.Length);
+                        lastPeoplePathIndex = Random.Range((int)0, 3);
+
+                        currPeopleStringLength = 0;
                     }
                     else
                     {
                         // Gen person
-                        //int rand = Random.Range((int)0, 100);
-                        //if(rand =)
+                        
                         GameObject peep = GameObject.Instantiate(peoplePrefabs[Random.Range(0, peoplePrefabs.Length)]);
                         PathFollower peepPF = peep.GetComponent<PathFollower>();
-                        peepPF.StartPath(paths[lastPeoplePathIndex]);
-                        personPath = lastPeoplePathIndex;
-                        peopleStringLength++;
+                        
+                        int rand = Random.Range((int)0, 100);
+                        if (rand < peopleSkewChance) // CHANCE TO VARIATE
+                        {
+                            personPath = (lastPeoplePathIndex + Random.Range((int) 0, 2)) % 3;
+
+                        }
+                        else
+                        {
+                            personPath = lastPeoplePathIndex;
+                        }
+
+                        lastPeoplePathIndex = personPath;
+                        
+                        peepPF.StartPath(paths[personPath]);
+                        currPeopleStringLength++;
                     }
                 }
                 else
                 {
                     peopleStringGap--;
                 }
+                
+                // OBSTACLES
+                // Generate something.
+                if (obstacleStringGap <= 0)
+                {
+                    if (currObstacleStringLength >= obstacleStringLength)
+                    {
+                        obstacleStringLength = Random.Range((int) 3, 7);
+                        obstacleStringGap = Random.Range((int) 3, 6);
+                        lastObstaclePathIndex = Random.Range((int)0, 3);
+                        if (lastObstaclePathIndex == personPath)
+                        {
+                            lastObstaclePathIndex = (lastObstaclePathIndex + 1) % 3;
+                        }
+
+                        currPeopleStringLength = 0;
+                    }
+                    else
+                    {
+                        // Gen person
+                        
+                        GameObject obst = GameObject.Instantiate(obstaclePrefabs[Random.Range(0, obstaclePrefabs.Length)]);
+                        PathFollower obstPF = obst.GetComponent<PathFollower>();
+                        
+                        int rand = Random.Range((int)0, 100);
+                        if (rand < obstacleSkewChance) // CHANCE TO VARIATE
+                        {
+                            obstaclePath = (lastObstaclePathIndex + Random.Range((int) 0, 2)) % 3;
+
+                        }
+                        else
+                        {
+                            obstaclePath = lastObstaclePathIndex;
+                        }
+
+                        lastObstaclePathIndex = obstaclePath;
+                        
+                        if (lastObstaclePathIndex == personPath)
+                        {
+                            lastObstaclePathIndex = (lastObstaclePathIndex + 1) % 3;
+                        }
+                        
+                        obstPF.StartPath(paths[lastObstaclePathIndex]);
+                        currObstacleStringLength++;
+                        
+                        // DOUBLE UP
+                        int rand2 = Random.Range((int)0, 100);
+                        if (rand2 < obstacleDoubleChance) // CHANCE TO DOUBLE
+                        {
+                            int dubPath = (lastObstaclePathIndex + 1) % 3;
+                            if (dubPath == personPath || dubPath == obstaclePath)
+                            {
+                                dubPath = (dubPath + 1) % 3;
+                                if (dubPath == personPath || dubPath == obstaclePath)
+                                {
+                                    dubPath = (dubPath + 1) % 3;
+                                }
+                            }
+                            
+                            GameObject obst2 = GameObject.Instantiate(obstaclePrefabs[Random.Range(0, obstaclePrefabs.Length)]);
+                            PathFollower obstPF2 = obst2.GetComponent<PathFollower>();
+                            obstPF2.StartPath(paths[dubPath]);
+                        }
+                    }
+                }
+                else
+                {
+                    obstacleStringGap--;
+                }
+                
             }
+            
             
             
 
